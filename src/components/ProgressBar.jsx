6fe2803767bdd5d75f34f7f0d356200ab1994ea1
essaisa <essaisa99@gmail.com>
2025-10-07 18:55:32 +0100

    Stats portion of dashboard complete

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -5,14 +5,14 @@ export default function ProgressBar() {
   const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   
   return (
-    <div className='Level'>
+    <div className='level'>
       <div>
         <h4>{text}</h4>
       </div>
 
       {arr.map((element, eleIdx) => {
         return (
-          <div className='level-bar' key={eleIdx}>{element}</div>
+          <div className='level-bar' key={eleIdx}>{}</div>
         )
       })}
 
